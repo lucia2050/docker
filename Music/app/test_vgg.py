3cadafe6fbfd789b1e91b3743a1a5761d7910467
lucia2050 <99085650+lucia2050@users.noreply.github.com>
2023-12-13 20:20:17 +0100

    Update and rename test.py to test_vgg.py

--- Music/app/test_vgg.py
+++ Music/app/test_vgg.py
@@ -9,14 +9,6 @@ class TestPredictions(unittest.TestCase):
     def extract_label(self, filename):
         # Extract class label (e.g., 'disco' or 'pop') from the filename
         return os.path.splitext(filename)[0].split('.')[0]
-    
-    '''def test_svm_predictions(self):
-        wav_files = ['pop.00003.wav']  # Add more files if necessary
-        for wav_file in wav_files:
-            prediction = predict_svm(f'/app/test_data/{wav_file}')
-            label = self.extract_label(wav_file)
-            self.assertIn(label,prediction)'''
-           
 
     def test_vgg_predictions(self):
         wav_files = ['disco.00004.wav']  # Add more files if necessary
