3df1ae50c72853e4362a11e839343a8098499904
lucia2050 <99085650+lucia2050@users.noreply.github.com>
2023-12-13 19:38:40 +0100

    Update test.py

--- Music/app/test.py
+++ Music/app/test.py
@@ -19,14 +19,14 @@ class TestPredictions(unittest.TestCase):
            
 
     def test_vgg_predictions(self):
-    wav_files = ['disco.00004.wav']  # Add more files if necessary
-    for wav_file in wav_files:
-        file_path = f'/app/test_data/{wav_file}'
-        with open(file_path, 'rb') as wav_file:
-            vgg_response = requests.post(vgg_url, files={'audio': wav_file})
-        vgg_prediction = vgg_response.json()['prediction'] if vgg_response.ok else 'Error predicting with VGG'
-        label = self.extract_label(wav_file)
-        self.assertIn(label, vgg_prediction)
+        wav_files = ['disco.00004.wav']  # Add more files if necessary
+        for wav_file in wav_files:
+            file_path = f'/app/test_data/{wav_file}'
+            with open(file_path, 'rb') as wav_file:
+                vgg_response = requests.post(vgg_url, files={'audio': wav_file})
+            vgg_prediction = vgg_response.json()['prediction'] if vgg_response.ok else 'Error predicting with VGG'
+            label = self.extract_label(wav_file)
+            self.assertIn(label, vgg_prediction)
            
 
 if __name__ == '__main__':
