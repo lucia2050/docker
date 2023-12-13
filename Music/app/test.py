413e481ecfaf8915bf31e07473235979f484897e
lucia2050 <99085650+lucia2050@users.noreply.github.com>
2023-12-13 19:18:27 +0100

    Update test.py

--- Music/app/test.py
+++ Music/app/test.py
@@ -2,26 +2,31 @@ import unittest
 import os
 from svm_service import predict_svm
 from vgg19_service import predict_vgg
+import requests
+vgg_url = 'http://vgg:5000/predict_vgg'
 
 class TestPredictions(unittest.TestCase):
     def extract_label(self, filename):
         # Extract class label (e.g., 'disco' or 'pop') from the filename
         return os.path.splitext(filename)[0].split('.')[0]
     
-    def test_svm_predictions(self):
+    '''def test_svm_predictions(self):
         wav_files = ['pop.00003.wav']  # Add more files if necessary
         for wav_file in wav_files:
             prediction = predict_svm(f'/app/test_data/{wav_file}')
             label = self.extract_label(wav_file)
-            self.assertIn(label,prediction)
+            self.assertIn(label,prediction)'''
            
 
     def test_vgg_predictions(self):
         wav_files = ['disco.00004.wav']  # Add more files if necessary
         for wav_file in wav_files:
-            prediction = predict_vgg(f'/app/test_data/{wav_file}')
+             file_path= '/app/test_data/{wav_file}'
+            with open(file_path, 'rb') as wav_file:
+               vgg_response = requests.post(vgg_url, files={'audio': wav_file})
+            vgg_prediction = vgg_response.json()['prediction'] if vgg_response.ok else 'Error predicting with VGG'
             label = self.extract_label(wav_file)
-            self.assertIn(label,prediction)
+            self.assertIn(label,vgg_prediction )
            
 
 if __name__ == '__main__':
